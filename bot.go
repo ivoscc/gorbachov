96b42c4718a402a32d968189803ad2d3d25fd6a8
ivoscc <ivoscc@gmail.com>
2013-08-10 20:59:13 -0500

    +fmt

--- bot.go
+++ bot.go
@@ -8,32 +8,32 @@ import (
 
 // Server message (http://tools.ietf.org/html/rfc1459.html#section-2.3)
 type Message struct {
-    Prefix string
-    Command string
-    Arguments []string
+	Prefix    string
+	Command   string
+	Arguments []string
 }
 
 // GetPrivMSG is a utility function to get the text portion of a Message.
 func (message *Message) GetPrivMSG() string {
-    return message.Arguments[len(message.Arguments)-1]
+	return message.Arguments[len(message.Arguments)-1]
 }
 
 type Bot struct {
-	server           string
-	name             string
-	channel          string
-	handlers         map[string]func(*Bot, Message)
-	client           *Client
+	server   string
+	name     string
+	channel  string
+	handlers map[string]func(*Bot, Message)
+	client   *Client
 }
 
 // CreateBot initializes a new Bot and returns a pointer to it.
 func CreateBot(server, name, channel string) *Bot {
-    handlers := make(map[string]func(*Bot, Message))
-    return &Bot{server, name, channel, handlers, &Client{}}
+	handlers := make(map[string]func(*Bot, Message))
+	return &Bot{server, name, channel, handlers, &Client{}}
 }
 
 func (bot *Bot) AddHandler(regex string, handler func(*Bot, Message)) {
-    bot.handlers[regex] = handler
+	bot.handlers[regex] = handler
 }
 
 func (bot *Bot) Say(message string) {
@@ -43,7 +43,7 @@ func (bot *Bot) Say(message string) {
 
 // Dispatches a handler function depending on the PRIVMSG received
 func (bot *Bot) HandleMessage(message Message) {
-    arguments := message.Arguments
+	arguments := message.Arguments
 	for str, handler := range bot.handlers {
 		matches, err := regexp.MatchString(str, arguments[len(arguments)-1])
 
--- client.go
+++ client.go
@@ -1,35 +1,34 @@
 package gorbachov
 
 import (
-    "fmt"
-    "log"
-    "net/textproto"
+	"fmt"
+	"log"
+	"net/textproto"
 )
 
 // The IRC client
 type Client struct {
-    connection *textproto.Conn
+	connection *textproto.Conn
 }
-
 
 // SendResponse sends a 'message' string to the client's connection.
 func (client *Client) SendResponse(message string) {
-    log.Printf("=> %v", message)
-    connection := client.connection
-    _, err := connection.Cmd(message)
-    if err != nil {
-        log.Fatal(err)
-    }
+	log.Printf("=> %v", message)
+	connection := client.connection
+	_, err := connection.Cmd(message)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // ReadLine reads a line from the open connection and prints it.
 func (client *Client) ReadLine() string {
-    line, err := client.connection.ReadLine()
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Printf("<= %v\n", line)
-    return line
+	line, err := client.connection.ReadLine()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("<= %v\n", line)
+	return line
 }
 
 // Connect opens a connection to an IRC server
@@ -37,62 +36,62 @@ func (client *Client) Connect(server string) {
 
 	conn, err := textproto.Dial("tcp", server)
 	if err != nil {
-        log.Fatal(err)
+		log.Fatal(err)
 	}
-    client.connection = conn
+	client.connection = conn
 }
 
 // Start dispatches the client and returns a channel to receive messages from
 func (client *Client) Start(botname, channel string) chan Message {
 
-    established := make(chan bool)
+	established := make(chan bool)
 
-    // TODO: timeout
-    go func(established chan bool) {
-        for {
-            _, command, _, err := ParseMessage(client.ReadLine())
-            if err != nil {
-                log.Fatalf("Error while parsing received message")
-            }
-            if command == "MODE" {
-                break
-            }
-        }
-        established <- true
-    }(established)
+	// TODO: timeout
+	go func(established chan bool) {
+		for {
+			_, command, _, err := ParseMessage(client.ReadLine())
+			if err != nil {
+				log.Fatalf("Error while parsing received message")
+			}
+			if command == "MODE" {
+				break
+			}
+		}
+		established <- true
+	}(established)
 
-    client.SendResponse(fmt.Sprintf("NICK %v", botname))
-    client.SendResponse(fmt.Sprintf("USER %v 0 * %v", botname, botname))
-    client.JoinChannel(channel)
+	client.SendResponse(fmt.Sprintf("NICK %v", botname))
+	client.SendResponse(fmt.Sprintf("USER %v 0 * %v", botname, botname))
+	client.JoinChannel(channel)
 
-    <-established
+	<-established
 
-    message_stream := make(chan Message)
-    go client.Run(message_stream)
-    return message_stream
+	message_stream := make(chan Message)
+	go client.Run(message_stream)
+	return message_stream
 }
 
 func (client *Client) Run(message_stream chan Message) {
 
-    for {
-        prefix, command, arguments, err := ParseMessage(client.ReadLine())
-        if err != nil {
-            log.Fatalf("Error while parsing received message")
-        }
-        if command == "PRIVMSG" {
-            message_stream <- Message{prefix, command, arguments}
-        } else if command == "PING" {
-            client.Execute(fmt.Sprintf("PONG :%v", arguments[0]))
-        }
-    }
+	for {
+		prefix, command, arguments, err := ParseMessage(client.ReadLine())
+		if err != nil {
+			log.Fatalf("Error while parsing received message")
+		}
+		if command == "PRIVMSG" {
+			message_stream <- Message{prefix, command, arguments}
+		} else if command == "PING" {
+			client.Execute(fmt.Sprintf("PONG :%v", arguments[0]))
+		}
+	}
 }
 
 func (client *Client) JoinChannel(channel string) {
-    // TODO: Validate channel (#, &, etc)
-    client.SendResponse(fmt.Sprintf("JOIN %v", channel))
+	// TODO: Validate channel (#, &, etc)
+	client.SendResponse(fmt.Sprintf("JOIN %v", channel))
 }
 
 // Execute sends an arbitrary command to the server (no validity checking is done)
 func (client *Client) Execute(command string) {
-    client.SendResponse(command)
+	client.SendResponse(command)
 }
